90acda05633a0cd904a78683a920b4ad379b4458
brad <bradleynielsen@gmail.com>
2022-04-23 04:28:17 -0500

    working xml parsing

---
+++ run.ps1
@@ -0,0 +1,61 @@
+﻿#init
+
+
+$scriptRootPath     = $PSScriptRoot
+#get list of ckl files
+
+$cklFiles = Get-ChildItem "$scriptRootPath\ckl"
+
+
+    foreach ($file in $cklFiles){
+        #check if file is CKL
+        if([System.IO.Path]::GetExtension($file) -eq ".ckl" ){
+            
+            [xml]$xmlDocument = get-content $file.FullName
+
+            ([xml]$xmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_IP').Node.innerxml 
+            
+             
+
+
+
+
+
+
+        }
+
+        
+        
+    
+    
+    }
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for each ckl file, find the respective xccdf
+
+
+<#
+
+
+             $filePath = $file.FullName
+             $filePath 
+             Select-Xml -Path $filePath -XPath \\CHECKLIST
+
+
+
+#>
