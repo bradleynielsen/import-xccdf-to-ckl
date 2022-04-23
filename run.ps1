cb0882e6f33459287a9707cdb800955256f76aff
brad <bradleynielsen@gmail.com>
2022-04-23 15:10:41 -0500

    ckl working, xccdf not working

--- run.ps1
+++ run.ps1
@@ -4,37 +4,81 @@
 $scriptRootPath     = $PSScriptRoot
 #get list of ckl files
 
-$cklFiles = Get-ChildItem "$scriptRootPath\ckl"
+$cklFiles   = Get-ChildItem "$scriptRootPath\ckl"
+$xccdfFiles = Get-ChildItem "$scriptRootPath\xccdf"
 
 
-    foreach ($file in $cklFiles){
-        #check if file is CKL
-        if([System.IO.Path]::GetExtension($file) -eq ".ckl" ){
+foreach ($cklFile in $cklFiles){
+    #check if file is CKL
+    if([System.IO.Path]::GetExtension($cklFile) -eq ".ckl" ){
+        #get xml data for ckl            
+        [xml]$cklXmlDocument = get-content $cklFile.FullName
+
+        #get host information
+        $cklHOST_NAME = ([xml]$cklXmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_NAME').Node.innerxml 
+        $cklHOST_IP   = ([xml]$cklXmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_IP').Node.innerxml 
+        $cklHOST_MAC  = ([xml]$cklXmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_MAC').Node.innerxml 
+        $cklHOST_FQDN = ([xml]$cklXmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_FQDN').Node.innerxml 
+        
+        #"getting ckl for $cklHOST_IP"
             
-            [xml]$xmlDocument = get-content $file.FullName
-            #get host information
-            $HOST_NAME = ([xml]$xmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_NAME').Node.innerxml 
-            $HOST_IP   = ([xml]$xmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_IP').Node.innerxml 
-            $HOST_MAC  = ([xml]$xmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_MAC').Node.innerxml 
-            $HOST_FQDN = ([xml]$xmlDocument | Select-Xml -XPath 'CHECKLIST/ASSET/HOST_FQDN').Node.innerxml 
+
+
+        #find the matching xccdf file
+
+
+        foreach ($xccdfFile in $xccdfFiles){
+            if([System.IO.Path]::GetExtension($xccdfFile) -eq ".xml" ){
+                #"getting xccdf"
+
+
+
+
+                [xml]$xccdfXmlDocument = get-content $xccdfFile.FullName
+                #$xpath = "/Benchmark/'status date'"
+                $xpath = "/Benchmark"
+                
+                
+
+
+                [xml]$xccdfXmlDocument | Select-Xml -XPath $xpath | select-object -expandProperty Node
+
+
+                ([xml]$xccdfXmlDocument | Select-Xml -XPath $xpath   ).Node.InnerText
+
+
+
+
+
+
+
+
+                
+                #([xml]$xccdfXmlDocument | Select-Xml -XPath $xpath) | Select-Object -ExpandProperty *
+
+                
+
+                
+                
             
-            $HOST_NAME
-            $HOST_IP 
-            $HOST_MAC 
-            $HOST_FQDN 
+            
+            
+            }
+            
 
-
-
-
-
-
+        
+        
         }
 
+
         
-        
-    
-    
+
+
     }
+    
+}
+
+
 
 
 
@@ -44,24 +88,3 @@ $cklFiles = Get-ChildItem "$scriptRootPath\ckl"
 
 
 
-
-
-
-
-
-
-
-
-#for each ckl file, find the respective xccdf
-
-
-<#
-
-
-             $filePath = $file.FullName
-             $filePath 
-             Select-Xml -Path $filePath -XPath \\CHECKLIST
-
-
-
-#>
